32d565ae3429d20be6bd9a88335e32890799199f
Nazar-bnm <nazar.bnm@gmail.com>
2017-09-30 19:05:09 +0300

    add method for get posts

--- src/pages/feed/FeedPage.js
+++ src/pages/feed/FeedPage.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
 import { PageLayout } from '../../shared/components/PageLayout'
+import agent from '../../agent'
 import './FeedPage.scss'
 
 export class FeedPage extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      postsPage: 0,
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    const {token, id} = this.props;
+
+    agent.Posts.all(id, this.state.postsPage, token).then((posts) => {
+      this.setState({
+        posts
+      })
+      console.log('posts response', posts)
+      console.log('posts', this.state.posts);
+    })
+  }
+
   render () {
     return (
       <PageLayout>
--- src/pages/feed/FeedPageContainer.js
+++ src/pages/feed/FeedPageContainer.js
@@ -2,6 +2,13 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { FeedPage } from './FeedPage'
 
+const mapStateToProps = state => {
+  return {
+    token: state.auth.user.token,
+    id: state.auth.user.id
+  }
+};
+
 export const FeedPageContainer = connect(
-  null
+  mapStateToProps
 )(FeedPage)
