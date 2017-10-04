466b828393dc26634c7214c079616ab00ac12722
Volodymyr Sen <vsen@griddynamics.com>
2017-10-04 22:14:35 +0200

    Fixed localization message

--- src/localization/messages.js
+++ src/localization/messages.js
@@ -3,7 +3,11 @@ export const messages = {
     landing: {
       banner: {
         main: 'Four paws, one loving heart',
-        sub: 'Every dog should have a place where it can feel safe.'
+        sub: 'Every dog should have a place where it can feel safe.',
+      },
+      feedback: {
+        first: 'Join largest pet owner community in Ukraine. Our mission is to build community of people that share attitude and devotion each of us have to our little 4 pawed friends. The community that provides helping hand in need, shares our brightest moments, friends with the same sphere of interests.',
+        second: 'All our efforts are targeted to make pet\'s and owner\'s life easier and better. Our goal is to build network of professional pet services around pet owners, providing good quality and quick services in your neighborhood. Looking for a groomer, trainer or day care for your puppy, Pawsom is the place where pet owners get connected to pet lovers.'
       }
     },
 
@@ -19,13 +23,6 @@ export const messages = {
 
     feedback: {
       title: 'Meet the animal lovers who will treat your pet like family'
-    },
-
-    landing:{
-      feedback:{
-        first: 'Join largest pet owner community in Ukraine. Our mission is to build community of people that share attitude and devotion each of us have to our little 4 pawed friends. The community that provides helping hand in need, shares our brightest moments, friends with the same sphere of interests.',
-        second: 'All our efforts are targeted to make pet\'s and owner\'s life easier and better. Our goal is to build network of professional pet services around pet owners, providing good quality and quick services in your neighborhood. Looking for a groomer, trainer or day care for your puppy, Pawsom is the place where pet owners get connected to pet lovers.'
-      }
     }
   },
 
@@ -33,7 +30,11 @@ export const messages = {
     landing: {
       banner: {
         main: 'Четыре лапы, одно любящее сердце',
-        sub: 'У каждой собаки должно быть место, где она может чувствовать себя в безопасности.'
+        sub: 'У каждой собаки должно быть место, где она может чувствовать себя в безопасности.',
+      },
+      feedback: {
+        first: 'Присоединяйтесь к самому большому сообществу любителей домашних животных в Украине. Наша миссия построить сообщество людей которые розделяют отношение, преданость и любовь к черырелапым друзьям. Сообщество которое продоставляет руку помощи в нужный момент, розделяют лучшие моменты жизни, друзья у которых общие интересы.',
+        second: 'Все наши усилия направлены на улучшение жизни любителей животных и их питомцев. Наша цель построить сеть профисиональних услуг вокруг владельцев домашних животных, предлагая качествиные и быстрые услуги в вашем раёне. Вашемо питомцу нужна стрижка, тренер или передержка для щенка - Pawsom объеденяет владельцев домашних животных и желающих помочь.'
       }
     },
 
@@ -49,13 +50,6 @@ export const messages = {
 
     feedback: {
       title: 'Познакомьтесь с любителями животных, которые будут относиться к вашему питомцу, как к семье'
-    },
-
-    landing:{
-      feedback:{
-        first: 'Присоединяйтесь к самому большому сообществу любителей домашних животных в Украине. Наша миссия построить сообщество людей которые розделяют отношение, преданость и любовь к черырелапым друзьям. Сообщество которое продоставляет руку помощи в нужный момент, розделяют лучшие моменты жизни, друзья у которых общие интересы.',
-        second: 'Все наши усилия направлены на улучшение жизни любителей животных и их питомцев. Наша цель построить сеть профисиональних услуг вокруг владельцев домашних животных, предлагая качествиные и быстрые услуги в вашем раёне. Вашемо питомцу нужна стрижка, тренер или передержка для щенка - Pawsom объеденяет владельцев домашних животных и желающих помочь.'
-      }
     }
   },
 
@@ -63,7 +57,11 @@ export const messages = {
     landing: {
       banner: {
         main: 'Чотири лапи, одне любляче серце',
-        sub: 'У кожної собаки повинно бути місце, де вона може відчувати себе в безпеці.'
+        sub: 'У кожної собаки повинно бути місце, де вона може відчувати себе в безпеці.',
+      },
+      feedback: {
+        first: 'Приеднуйтеся до найбільшої спільноти власників домашніх тварин в Україні. Наша міссія побудувати спільноту людей котрі розділяють відношшення, прихильність та любов до чотирилапих друзів. Спільнота котра допоможе у скрутний момент, розділить найкращі моменти у житті, друзі зі спільними зацікавленнями.',
+        second: 'Усі наші зусилля направлені на покращення життя любителів тварин і їх улюбленців. Наша ціль побудувати мережу професійних послуг навколо власників домашніх тварин, надаючи якісні та швидкі послуги у вашому районі. Вашому улюбленцю потрібна стрижка, тренер чи перетримка для цуцика на день, Pawsom об\'єднює власників домашніх тварин та охочих допомогти.'
       }
     },
 
@@ -79,13 +77,6 @@ export const messages = {
 
     feedback: {
       title: 'Знайомтесь з любителями тварин, які будуть поводитися з вашим улюбленцем як сім\'я'
-    },
-
-    landing:{
-      feedback:{
-        first: 'Приеднуйтеся до найбільшої спільноти власників домашніх тварин в Україні. Наша міссія побудувати спільноту людей котрі розділяють відношшення, прихильність та любов до чотирилапих друзів. Спільнота котра допоможе у скрутний момент, розділить найкращі моменти у житті, друзі зі спільними зацікавленнями.',
-        second: 'Усі наші зусилля направлені на покращення життя любителів тварин і їх улюбленців. Наша ціль побудувати мережу професійних послуг навколо власників домашніх тварин, надаючи якісні та швидкі послуги у вашому районі. Вашому улюбленцю потрібна стрижка, тренер чи перетримка для цуцика на день, Pawsom об\'єднює власників домашніх тварин та охочих допомогти.'
-      }
     }
   }
 }
