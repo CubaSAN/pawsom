f8ee1d9adf9dcee6ddf0f1cd64075725c1ab69a8
Nazar-bnm <nazar.bnm@gmail.com>
2017-10-06 22:22:38 +0300

    add render post page

--- src/pages/post/PostPage.js
+++ src/pages/post/PostPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Col } from 'reactstrap'
 import PropTypes from 'prop-types'
+import TimeAgo from 'react-timeago'
 import { PageLayout } from '../../shared/components/PageLayout'
 import agent from '../../agent'
 import './PostPage.scss'
@@ -15,6 +16,10 @@ export class PostPage extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      post: null
+    }
   }
 
   componentWillMount() {
@@ -22,8 +27,42 @@ export class PostPage extends Component {
     const id = this.props.computedMatch.params.id
 
     agent.Posts.byId(id, token).then((post) => {
-      console.log(post);
+      this.setState({
+        post
+      })
     })
+  }
+
+  renderPost() {
+    const post = this.state.post
+
+    return (
+      post ?
+      <div>
+        <div className="post">
+          <div className="post-header">
+            <div className="post-user-container">
+              <div className="post-user-image">
+                 <img src={post.postedPersonString} alt="User"/>
+              </div>
+              <div className="post-user">
+                <div className="post-user-name">{post.postedPersonName}</div>
+                <div className="post-user-created">
+                  <TimeAgo date={post.created} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="post-content">
+              {post.url.length && <img className="post-image" src="https://static.pexels.com/photos/59523/pexels-photo-59523.jpeg" alt="" />}
+            <p className="post-text">{post.text}</p>
+          </div>
+          <div onClick={() => this.deletePost()}>Delete Post</div>
+        </div>
+      </div>
+      :
+      <div>post load...</div>
+    )
   }
 
   render () {
@@ -32,7 +71,12 @@ export class PostPage extends Component {
     return (
       <PageLayout className={CN}
         isPageAvailable={lat && lng}>
-        <h2>Post Page</h2>
+        <Col md={9}>
+          {this.renderPost()}
+        </Col>
+
+        <Col md={3}>
+        </Col>
       </PageLayout>
     )
   }
